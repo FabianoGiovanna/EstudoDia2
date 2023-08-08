8c634d6f474c529c99f9a43f95978808b158ec2b
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2023-08-08 14:00:08 +0100

    Add maximum and minimum numbers

---
+++ ValorerEspeciais/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ValorerEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intMaxValue = int.MaxValue;
+            int intMinValue = int.MinValue;
+            double doubleMaxValue = double.MaxValue;
+            double doubleMinValue = double.MinValue;
+            float floatMaxValue = float.MaxValue;
+            float floatMinValue = float.MinValue;
+
+            Console.WriteLine(intMaxValue);
+            Console.WriteLine(intMinValue);
+            Console.WriteLine(doubleMaxValue);
+            Console.WriteLine(doubleMinValue);
+            Console.WriteLine(floatMaxValue);
+            Console.WriteLine(floatMinValue);
+        }
+    }
+}
