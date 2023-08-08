e6fedf822e5944c3d90f12af527171dc5aaaf60c
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2023-08-08 13:47:24 +0100

    Add Cylinder Volume and Spuerficial Area completed

---
+++ Cilindro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            float volume, superficialArea;
+            float pi = 3.1415926f;
+
+            Console.WriteLine("Welcome to cylinder calculator\n");
+            Console.WriteLine("Insert height: ");
+            string height = Console.ReadLine();
+            float heightConvert = float.Parse(height);
+            Console.WriteLine("Insert radius: ");
+            string radius = Console.ReadLine();
+            float radiusConvert = float.Parse(radius);
+
+            volume = pi * (radius * radius) * height;
+            superficialAreaArea = 2 * pi * radius * (radius + height);
+
+            Console.WriteLine("Volume = {volume}     Superficial Area = {superficialArea}" ,volume, superficialArea);
+
+
+
+            
+        }
+    }
+}
