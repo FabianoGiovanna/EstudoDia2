37962c6323c9276a018bcb3e237a11450b77578f
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2023-08-08 12:58:59 +0100

    Add sufixe --

--- IncDec/Program.cs
+++ IncDec/Program.cs
@@ -7,9 +7,12 @@ namespace IncDec
         static void Main(string[] args)
         {
             int number1 = 0;
+            int number2 = 1;
 
             number1++;
+            number2--;
             Console.WriteLine(number1);
+            Console.WriteLine(number2);
         }
     }
 }
