31bcc3b860e2b815e20f50eea9b576baf88de153
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2023-08-08 14:58:22 +0100

    Add Piramide

---
+++ Piramide/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Piramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Insert a number...");
+            string number = Console.ReadLine();
+            int val = int.Parse(number);
+            
+        for (int i = 0; i < val; i++)
+        {
+            Console.WriteLine(new String(' ', val-i-1) + new String('*', i+i+1));
+        }
+            Console.ReadLine();
+        }
+    }
+}
